a3fe92f8341773d754a40c36cd880e9ef93f1a15
Lourdes Reyes <lourdes.reyes@utexas.edu>
2023-01-13 12:10:34 -0600

    More compact code
    keeping left, right, forward, back
    simplified code

--- vibration_test_f/vibration_test_f.ino
+++ vibration_test_f/vibration_test_f.ino
@@ -5,11 +5,9 @@ int vsc = 9;
 int vsd = 5;
 int vse = 4;
 int vsf = 6;
-//int vs7 = 5;
-//int vs8 = 3;
+
 int pwm_power = 150;  // out of 255
 int pwm_power_low = 0;
-int input = 0;  // for incoming serial data
 int delay_time = 300;
 // ----------- Arduino -----------
 void setup() {
@@ -17,32 +15,11 @@ void setup() {
 }
 
 void loop() {
-  // print to vibration motors
-  // one side at a time
 
   //wait for user input
-  while (Serial.available() == 0) {
-    /*
-      analogWrite(vsa, pwm_power_low);
-      analogWrite(vsb, pwm_power_low);
-      analogWrite(vsc, pwm_power_low);
-      analogWrite(vsd, pwm_power_low);
-      analogWrite(vse, pwm_power_low);
-      analogWrite(vsf, pwm_power_low);
-      analogWrite(vs7, pwm_power_low);
-      analogWrite(vs8, pwm_power_low);
-    */
-  }
-  analogWrite(vsa, pwm_power_low);
-  analogWrite(vsb, pwm_power_low);
-  analogWrite(vsc, pwm_power_low);
-  analogWrite(vsd, pwm_power_low);
-  analogWrite(vse, pwm_power_low);
-  analogWrite(vsf, pwm_power_low);
-
+  while (Serial.available() == 0) { }
 
   int sequenceChoice = Serial.parseInt();
-   //char sequenceChoice = Serial.read();
 
   switch (sequenceChoice) {
     case 1: // Left
@@ -62,14 +39,6 @@ void loop() {
       delay(delay_time);
       break;
     case 3: //Forward
-      analogWrite(vsa, pwm_power);
-      delay(delay_time);
-      analogWrite(vsb, pwm_power);
-      delay(delay_time);
-      analogWrite(vsc, pwm_power);
-      delay(delay_time);
-      break;
-    case 4: // Back
       analogWrite(vsc, pwm_power);
       delay(delay_time);
       analogWrite(vsb, pwm_power);
@@ -77,20 +46,14 @@ void loop() {
       analogWrite(vsa, pwm_power);
       delay(delay_time);
       break;
-      /*
-        case 5: //Up
-        analogWrite(vs7, pwm_power);
-        delay(delay_time);
-        analogWrite(vsb, pwm_power);
-        delay(delay_time);
-        case 6: //Down
-        analogWrite(vsb, pwm_power);
-        delay(delay_time);
-        analogWrite(vs7, pwm_power);
-        delay(delay_time);
-        case 9: // Test pins
-        analogWrite(vsa, pwm_power);
-      */
+    case 4: // Back  
+      analogWrite(vsa, pwm_power);
+      delay(delay_time);
+      analogWrite(vsb, pwm_power);
+      delay(delay_time);
+      analogWrite(vsc, pwm_power);
+      delay(delay_time);
+      break;
   }
 
   analogWrite(vsa, pwm_power_low);
@@ -99,6 +62,4 @@ void loop() {
   analogWrite(vsd, pwm_power_low);
   analogWrite(vse, pwm_power_low);
   analogWrite(vsf, pwm_power_low);
-  //analogWrite(vs7, pwm_power_low);
-  //analogWrite(vs8, pwm_power_low);
 }
